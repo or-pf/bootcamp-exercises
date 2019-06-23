65a9ae7f53f843084ed75153ea648c9b4f6e55f3
o.p <ornapf@gmail.com>
2019-06-23 08:53:14 +0300

    Js testing ex3

--- W6D3-JStests/ex3/tests/test1.js
+++ W6D3-JStests/ex3/tests/test1.js
@@ -49,3 +49,27 @@ function buy(candyStore, id) {
     }
 }
 
+
+it('should return a candy info', function(){
+expect(getCandy(candyStore, "as12f")).to.deep.equal({name:"mint gum", id:"as12f", price:2, amount:2});
+});
+
+it('should return the price according to a given id', function () {
+    expect(getPrice(candyStore, "as12f")).to.equal(2);
+    expect(getPrice(candyStore, "5hd7y")).to.equal(5);
+});
+
+it('should add a new candy to the store', function () {
+    addCandy(candyStore, "mnm11", "m&m", 3.5);
+     expect(getCandy(candyStore, "mnm11")).to.deep.equal({name:"m&m", id:"mnm11", price:3.5, amount:1});
+});
+
+it('should reduce the candy amount by 1', function () {
+    const candyId = "as12f";
+    const candyAmount = getCandy(candyStore, candyId).amount;
+    
+    buy(candyStore, candyId);
+    
+    expect(getCandy(candyStore, candyId).amount).to.equal(candyAmount - 1);
+});
+
