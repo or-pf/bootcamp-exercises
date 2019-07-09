432971659a2b059b827d69d16e58290d701d612b
o.p <ornapf@gmail.com>
2019-07-09 17:04:40 +0300

    python intro booster ex completed

---
+++ W9D2-python_Intro/boosterEx.py
@@ -0,0 +1,11 @@
+const= "Hello, my name is Inigo Montoya"
+#      "H-l-o- -y-n-m- -s-I-i-o-M-n-o-a
+#        "l-o- -y-n-m- -s-I"
+one= const[0:5]
+two= const[7:14]
+three= const[0::2]
+four= const[2:-12:2]
+print (one)
+print (two)
+print (three)
+print (four)
