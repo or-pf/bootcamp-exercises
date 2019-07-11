a162fdc764ea4557876ba2115e60b70a9a24b67e
o.p <ornapf@gmail.com>
2019-07-11 13:16:02 +0300

    speaking python ex2 completed

---
+++ W9D3-speaking_python/exex.py
@@ -0,0 +1,8 @@
+info = {'name': "Israel Tech Challenge",
+'address': "Herzl 16 Tel Aviv",
+'is_non_profit': True,
+'num_of_programs': 5
+}
+print(info.keys())
+print(info.values())
+print(info.items())
