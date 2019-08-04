5ab77542f407272291c6b52707fa66ec75166fce
o.p <ornapf@gmail.com>
2019-08-04 17:12:16 +0300

    python environment ex1 now working

--- W10D1-python_environment/ex1.py
+++ W10D1-python_environment/ex1.py
@@ -7,7 +7,7 @@ print(sys.builtin_module_names)
 # 2. Import 3 built-in modules.
 import marshal
 import time
-import errno
+import math
 
 # 3. import 1 function from another built-in module
 import zlib
@@ -15,11 +15,11 @@ from zlib import compress
 
 # 4. In 1 line import 3 functions from a built-in module
 from math import sqrt, cos, degrees
-# 5. use a universal import to import all functions of a built-in module
 
+# 5. use a universal import to import all functions of a built-in module
 from math import *
 
 
-assert(calcFactorial(5) == 120)
-print(exponent(3))
-print(ceiling(3.2))
+assert(factorial(5) == 120)
+print(exp(3))
+print(ceil(3.2))
