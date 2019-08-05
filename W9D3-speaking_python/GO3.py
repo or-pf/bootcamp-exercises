499d36e3037dfc8f4b4d68cf955b414fbfa04d6d
o.p <ornapf@gmail.com>
2019-08-05 10:28:45 +0300

    speaking python ex GO3 done

--- W9D3-speaking_python/GO3.py
+++ W9D3-speaking_python/GO3.py
@@ -1,2 +1,16 @@
 def summer(listi):
-    if type
+    try:
+        for a in listi:
+            if isinstance(a, str):
+                new_list = "".join(listi)
+            if isinstance(a, int):
+                new_list = sum(listi)
+        print(new_list)
+        return new_list
+    except TypeError:
+        print("can't add string and number")
+
+summer(["a", "b", "c"])
+summer([1,3,10])
+summer(["ag", "tr", "h"])
+summer([1, "tr", "h"])
